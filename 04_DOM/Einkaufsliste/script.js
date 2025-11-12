382d86b089cf1045949ee781218bea949c9c5ea1
Samuel Ming <samuelming09@gmail.com>
2025-11-12 12:44:46 +0100

    Initial Commit

--- 04_DOM/Einkaufsliste/script.js
+++ 04_DOM/Einkaufsliste/script.js
@@ -1,6 +1,23 @@
+const btn = document.getElementById('add-btn');
+btn.addEventListener('click', addToList)
+
 window.addEventListener("load", initialize);
 
 function initialize() {
    alert('Guten Tag und viel Spass mit der Einkaufsliste :-)');
 }
- 
+
+function addToList() {
+    const li = document.createElement("li");
+    const ul = document.querySelector("ul");
+    const input = document.getElementById("add-input");
+
+    const value = input.value.trim();
+    if (value === "") {
+        return;
+    }
+
+    li.textContent = value;
+    ul.appendChild(li);
+    input.value = "";
+}
